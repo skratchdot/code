f7567b280d260c98a54edcab4efbd03cd59d9985
skratchdot <git@skratchdot.com>
2020-12-12 10:32:23 -0500

    🏴‍☠️ use a map of lamdas instead of a switch 🏴‍☠️

--- advent/2020/day/12/solve1.js
+++ advent/2020/day/12/solve1.js
@@ -1,82 +1,59 @@
 const { debug, run } = require('../lib');
 
 /*
-Action N means to move north by the given value.
-Action S means to move south by the given value.
-Action E means to move east by the given value.
-Action W means to move west by the given value.
-Action L means to turn left the given number of degrees.
-Action R means to turn right the given number of degrees.
-Action F means to move forward by the given value in the direction the 
+  Action N means to move north by the given value.
+  Action S means to move south by the given value.
+  Action E means to move east by the given value.
+  Action W means to move west by the given value.
+  Action L means to turn left the given number of degrees.
+  Action R means to turn right the given number of degrees.
+  Action F means to move forward by the given value in the direction the
 */
 
 const DIR_EAST = 'east';
 const DIR_NORTH = 'north';
 const DIR_WEST = 'west';
 const DIR_SOUTH = 'south';
+const COUNTER_CLOCKWISE = [DIR_EAST, DIR_NORTH, DIR_WEST, DIR_SOUTH];
+const CLOCKWISE = [DIR_EAST, DIR_SOUTH, DIR_WEST, DIR_NORTH];
+
+const turn = (arr, dir, val) => {
+  while (val > 0) {
+    dir = arr[(arr.indexOf(dir) + 1) % arr.length];
+    val -= 90;
+  }
+  if (val !== 0) {
+    throw new Error('you can only turn in increments of 90 degrees');
+  }
+  return dir;
+};
 
 const solve = (lines) => {
+  // instance variables
   let east = 0;
   let north = 0;
   let dir = DIR_EAST;
-
+  // execute all instructions
   lines.forEach((line) => {
     const [instruction, ...rest] = line.split('');
     let val = parseFloat(rest.join(''));
     debug({ east, north, dir, line, instruction, val });
-    switch (instruction) {
-      case 'N':
-        north += val;
-        break;
-      case 'S':
-        north -= val;
-        break;
-      case 'E':
-        east += val;
-        break;
-      case 'W':
-        east -= val;
-        break;
-      case 'L':
-        while (val > 0) {
-          if (dir === DIR_EAST) {
-            dir = DIR_NORTH;
-          } else if (dir === DIR_NORTH) {
-            dir = DIR_WEST;
-          } else if (dir === DIR_WEST) {
-            dir = DIR_SOUTH;
-          } else if (dir === DIR_SOUTH) {
-            dir = DIR_EAST;
-          }
-          val -= 90;
-        }
-        break;
-      case 'R':
-        while (val > 0) {
-          if (dir === DIR_EAST) {
-            dir = DIR_SOUTH;
-          } else if (dir === DIR_SOUTH) {
-            dir = DIR_WEST;
-          } else if (dir === DIR_WEST) {
-            dir = DIR_NORTH;
-          } else if (dir === DIR_NORTH) {
-            dir = DIR_EAST;
-          }
-          val -= 90;
-        }
-        break;
-      case 'F':
-        if (dir === DIR_EAST) {
-          east += val;
-        } else if (dir === DIR_NORTH) {
-          north += val;
-        } else if (dir === DIR_SOUTH) {
-          north -= val;
-        } else if (dir === DIR_WEST) {
-          east -= val;
-        }
-        break;
-    }
+    const moveForwardMap = {
+      [DIR_EAST]: () => (east += val),
+      [DIR_NORTH]: () => (north += val),
+      [DIR_SOUTH]: () => (north -= val),
+      [DIR_WEST]: () => (east -= val),
+    };
+    const instructionMap = {
+      N: () => (north += val),
+      S: () => (north -= val),
+      E: () => (east += val),
+      W: () => (east -= val),
+      L: () => (dir = turn(COUNTER_CLOCKWISE, dir, val)),
+      R: () => (dir = turn(CLOCKWISE, dir, val)),
+      F: () => moveForwardMap[dir](),
+    };
+    instructionMap[instruction]();
   });
   return Math.abs(east) + Math.abs(north);
 };
--- advent/2020/day/12/solve2.js
+++ advent/2020/day/12/solve2.js
@@ -1,21 +1,22 @@
 const { debug, run } = require('../lib');
 
 /*
-    Action N means to move the waypoint north by the given value.
-    Action S means to move the waypoint south by the given value.
-    Action E means to move the waypoint east by the given value.
-    Action W means to move the waypoint west by the given value.
-    Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
-    Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
-    Action F means to move forward to the waypoint a number of times equal to the given value.
+  Action N means to move the waypoint north by the given value.
+  Action S means to move the waypoint south by the given value.
+  Action E means to move the waypoint east by the given value.
+  Action W means to move the waypoint west by the given value.
+  Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+  Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+  Action F means to move forward to the waypoint a number of times equal to the given value.
 */
 
 const solve = (lines) => {
+  // instance variables
   let waypointEast = 10;
   let waypointNorth = 1;
   let shipEast = 0;
   let shipNorth = 0;
-
+  // execute all instructions
   lines.forEach((line) => {
     const [instruction, ...rest] = line.split('');
     let val = parseFloat(rest.join(''));
@@ -28,20 +29,12 @@ const solve = (lines) => {
       instruction,
       val,
     });
-    switch (instruction) {
-      case 'N':
-        waypointNorth += val;
-        break;
-      case 'S':
-        waypointNorth -= val;
-        break;
-      case 'E':
-        waypointEast += val;
-        break;
-      case 'W':
-        waypointEast -= val;
-        break;
-      case 'L':
+    const instructionMap = {
+      N: () => (waypointNorth += val),
+      S: () => (waypointNorth -= val),
+      E: () => (waypointEast += val),
+      W: () => (waypointEast -= val),
+      L: () => {
         while (val > 0) {
           let oldEast = waypointEast;
           let oldNorth = waypointNorth;
@@ -49,8 +42,8 @@ const solve = (lines) => {
           waypointEast = -oldNorth;
           val -= 90;
         }
-        break;
-      case 'R':
+      },
+      R: () => {
         while (val > 0) {
           let oldEast = waypointEast;
           let oldNorth = waypointNorth;
@@ -58,12 +51,13 @@ const solve = (lines) => {
           waypointEast = oldNorth;
           val -= 90;
         }
-        break;
-      case 'F':
+      },
+      F: () => {
         shipEast += val * waypointEast;
         shipNorth += val * waypointNorth;
-        break;
-    }
+      },
+    };
+    instructionMap[instruction]();
   });
   return Math.abs(shipEast) + Math.abs(shipNorth);
 };
