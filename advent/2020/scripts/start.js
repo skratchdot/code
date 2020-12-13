ca8b67fc678039c2913c7da988cb47e114d744d1
skratchdot <git@skratchdot.com>
2020-12-12 20:49:11 -0500

    npm start now watches for file changes and re-runs current day

--- advent/2020/scripts/start.js
+++ advent/2020/scripts/start.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
+const sane = require('sane');
+const util = require('util');
+const { spawn } = require('child_process');
 
 const year = new Date().getFullYear();
 const maxDay = 25;
@@ -15,10 +18,9 @@ const getDayFolder = (day) => {
   return dayFolder;
 };
 
-const writeTestFiles = async () => {
+const writeTestFiles = async (dayFolder) => {
   try {
     for (let day = 1; day <= currentDay && day <= maxDay; day++) {
-      const dayFolder = getDayFolder(day);
       const dayInput = path.resolve(dayFolder, 'input');
       if (!fs.existsSync(dayFolder)) {
         console.log(`Creating path:\n${dayFolder}`);
@@ -41,6 +43,31 @@ const writeTestFiles = async () => {
   }
 };
 
+const runTests = async (dayFolder) =>
+  new Promise((resolve) => {
+    const options = { stdio: 'inherit' };
+    // prettier-ignore
+    spawn('node', [path.resolve(dayFolder, 'solve1.js')], options).on('close', () => {
+        spawn('node', [path.resolve(dayFolder, 'solve2.js')], options).on('close', () => {
+            resolve();
+          }
+        );
+      }
+    );
+  });
+
 (async () => {
-  await writeTestFiles();
+  const dayFolder = getDayFolder(currentDay);
+  await writeTestFiles(dayFolder);
+  const watcher = sane(dayFolder);
+  watcher.on('ready', async () => {
+    console.log(`watching the "${dayFolder}" folder for changes.`);
+    await runTests(dayFolder);
+  });
+  ['change', 'add', 'delete'].forEach((eventName) => {
+    watcher.on(eventName, async (filepath) => {
+      console.log(`${eventName} event:\n${filepath}`);
+      await runTests(dayFolder);
+    });
+  });
 })().catch(console.error);
