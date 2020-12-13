2686ddac6848d0e26c5b241790da3a35ade5b2ff
skratchdot <git@skratchdot.com>
2020-12-13 17:23:10 -0500

    better watch mode 💻

--- advent/2020/scripts/start.js
+++ advent/2020/scripts/start.js
@@ -43,32 +43,76 @@ const writeTestFiles = async (dayFolder) => {
   }
 };
 
-const runTests = async (dayFolder) =>
-  new Promise((resolve) => {
+const runTests = async (dayFolder, reason) => {
+  console.clear();
+  if (reason) {
+    console.log(reason);
+  }
+  return new Promise((resolve) => {
     const options = { stdio: 'inherit' };
-    console.clear();
     // prettier-ignore
     spawn('node', [path.resolve(dayFolder, 'solve1.js')], options).on('close', () => {
         spawn('node', [path.resolve(dayFolder, 'solve2.js')], options).on('close', () => {
+          printInstructions();
             resolve();
           }
         );
       }
     );
   });
+};
+
+const printInstructions = () => {
+  console.log(`
+    DAY ${currentDay}:
+      press "q" or ctrl-c to quit
+      press "d" to toggle debug mode
+      any other key re-runs the tests
+    `);
+};
+
+// https://stackoverflow.com/a/12506613
+const setupKeyHandling = (dayFolder) => {
+  const stdin = process.stdin;
+  // without this, we would only get streams once enter is pressed
+  stdin.setRawMode(true);
+  // resume stdin in the parent process (node app won't quit all by itself
+  // unless an error or process.exit() happens)
+  stdin.resume();
+  // i don't want binary, do you?
+  stdin.setEncoding('utf8');
+  // on any data into stdin
+  stdin.on('data', async (key) => {
+    const check = (k) => key.toLowerCase() === k;
+    // ctrl-c ( end of text ) or q
+    if (key === '\u0003' || check('q')) {
+      process.exit();
+    } else if (check('d')) {
+      process.env.DEBUG = process.env.DEBUG === '*' ? undefined : '*';
+      await runTests(
+        dayFolder,
+        `debug mode toggled. process.env.DEBUG=${process.env.DEBUG}`
+      );
+    } else {
+      await runTests(dayFolder, `re-ran due to key: "${key}"`);
+    }
+  });
+};
 
 (async () => {
   const dayFolder = getDayFolder(currentDay);
   await writeTestFiles(dayFolder);
   const watcher = sane(dayFolder);
   watcher.on('ready', async () => {
-    console.log(`watching the "${dayFolder}" folder for changes.`);
-    await runTests(dayFolder);
+    await runTests(
+      dayFolder,
+      `watching the "${dayFolder}" folder for changes.`
+    );
   });
   ['change', 'add', 'delete'].forEach((eventName) => {
     watcher.on(eventName, async (filepath) => {
-      console.log(`${eventName} event:\n${filepath}`);
-      await runTests(dayFolder);
+      await runTests(dayFolder, `${eventName} event:\n${filepath}`);
     });
   });
+  setupKeyHandling(dayFolder);
 })().catch(console.error);
